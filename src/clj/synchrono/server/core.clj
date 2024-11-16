4fb8de8ffc77795c60ade21cee5c2788e2b552ee
H Z Wei <hzwei92@gmail.com>
2024-11-15 19:32:38 -0800

    try to demo

--- src/clj/synchrono/server/core.clj
+++ src/clj/synchrono/server/core.clj
@@ -93,7 +93,7 @@
   []
   (try
     (log/info "Initializing database")
-    (init-db!)
+    #_(init-db!)
     
     (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
       (start-server! port))
